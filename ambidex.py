def97d088e908d5629e3bf2d3e91febf45983625
Tyler Stephenson <tylerstephenson888@outlook.com>
2018-12-17 14:07:51 -0500

    added some more point assignment code, reimplemented roller.

--- ambidex.py
+++ ambidex.py
@@ -126,6 +126,24 @@ layout = "R"
 roundnum = 1
 screen = pg.display.set_mode((1280, 720))
 
+def abroller():
+    #RNG time
+    for index in range(0,8):
+        temp = random.randint(1, 100)
+        if temp <= 50:
+            playertable[index].vote = "A"
+        else:
+            playertable[index].vote = "B"
+        # check if a player is dead, change their vote to ally if they are
+        if not playertable[index].alive:
+            playertable[index].vote = "A"
+    if not playertable[0].alive:
+        playertable[0].vote = playertable[1].vote
+    if not playertable[2].alive:
+        playertable[2].vote = playertable[3].vote
+    if not playertable[4].alive:
+        playertable[4].vote = playertable[5].vote
+        
 def setup():
 
     pg.display.set_caption("Python Ambidex Game")
@@ -439,6 +457,7 @@ def layoutChooser():
     buttonGroup = pg.sprite.Group(buttonA,buttonB,buttonC)
     labelGroup = pg.sprite.Group(nvlText)
     keepGoing = True
+    abroller()
     while keepGoing:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -463,34 +482,94 @@ def layoutChooser():
         pg.mouse.set_visible(True)
         pg.display.flip()
 
-def ABbuttons():
-    pg.display.set_caption("Python Ambidex Game")
-    global screen
-
-    bg = pg.Surface(screen.get_size())
-    bg = bg.convert()
-    bg.fill((0,118,163))
-    ally = Button('A: ALLY', 640, 300)
-    betray = Button("B: BETRAY", 640, 500)
-
-    introButtons = pg.sprite.Group(ally,betray)
-    keepGoing = True
-    while keepGoing:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                keepGoing = False
-            if event.type == pg.MOUSEBUTTONDOWN:
-                        if ally.click():
-                            p1.vote = "A"
-                        if betray.click():
-                            p1.vote = 'B'
-        screen.blit(bg, (0, 0))
-        bg.blit(introButtons,(100,550))
-        introButtons.update()
-        introButtons.draw(screen)
-        pg.mouse.set_visible(True)
-        pg.display.flip()
-
+def pointAssignment():
+    global playertable
+    global layout
+    if layout.upper() == "A":
+        a = 0
+        b = 1
+        c = 7
+        d = 2
+        e = 3
+        f = 8
+        g = 4
+        h = 5
+        i = 6
+    elif layout.upper() == "B":
+        a = 0
+        b = 1
+        c = 8
+        d = 2
+        e = 3
+        f = 6
+        g = 4
+        h = 5
+        i = 7
+    elif layout.upper() == "C":
+        a = 0
+        b = 1
+        c = 6
+        d = 2
+        e = 3
+        f = 7
+        g = 4
+        h = 5
+        i = 8
+    if playertable[a].vote == "A" and playertable[c].vote == "A":
+        playertable[a].points = playertable[a].points + 2
+        playertable[b].points = playertable[b].points + 2
+        playertable[c].points = playertable[c].points + 2
+        print(playertable[a].role + " and " + playertable[c].role + " both allied. Both parties gain 2 points.")
+    elif playertable[a].vote == "A" and playertable[c].vote == "B":
+        playertable[a].points = playertable[a].points - 2
+        playertable[b].points = playertable[b].points - 2
+        playertable[c].points = playertable[c].points + 3
+        print(playertable[a].role + " allied while " + playertable[c].role + " betrayed. " + playertable[a].role + " loses 2 points while " + playertable[c].role + " gains 3.")
+    elif playertable[a].vote == "B" and playertable[c].vote == "A":
+        playertable[a].points = playertable[a].points + 3
+        playertable[b].points = playertable[b].points + 3
+        playertable[c].points = playertable[c].points - 2
+        print(playertable[c].role + " allied while " + playertable[a].role + " betrayed. " + playertable[c].role + " loses 2 points while " + playertable[a].role + " gains 3.")
+    else:
+        print(playertable[a].role + " and " + playertable[c].role + " both betrayed each other. Nothing happened with their scores.")
+    time.sleep(2)
+    if playertable[d].vote == "A" and playertable[f].vote == "A":
+        playertable[d].points = playertable[d].points + 2
+        playertable[e].points = playertable[e].points + 2
+        playertable[f].points = playertable[f].points + 2
+        print(playertable[d].role + " and " + playertable[f].role + " both allied. Both parties gain 2 points.")
+    elif playertable[d].vote == "A" and playertable[f].vote == "B":
+        playertable[d].points = playertable[d].points - 2
+        playertable[e].points = playertable[e].points - 2
+        playertable[f].points = playertable[f].points + 3
+        print(playertable[d].role + " allied while " + playertable[f].role + " betrayed. " + playertable[
+            a].role + " loses 2 points while " + playertable[f].role + " gains 3.")
+    elif playertable[d].vote == "B" and playertable[f].vote == "A":
+        playertable[d].points = playertable[d].points + 3
+        playertable[e].points = playertable[e].points + 3
+        playertable[f].points = playertable[f].points - 2
+        print(playertable[f].role + " allied while " + playertable[d].role + " betrayed. " + playertable[
+            c].role + " loses 2 points while " + playertable[d].role + " gains 3.")
+    else:
+        print(playertable[d].role + " and " + playertable[f].role + " both betrayed each other. Nothing happened with their scores.")
+    time.sleep(2)
+    if playertable[g].vote == "A" and playertable[i].vote == "A":
+        playertable[g].points = playertable[g].points + 2
+        playertable[h].points = playertable[h].points + 2
+        playertable[i].points = playertable[i].points + 2
+        print(playertable[g].role + " and " + playertable[i].role + " both allied. Both parties gain 2 points.")
+    elif playertable[g].vote == "A" and playertable[i].vote == "B":
+        playertable[g].points = playertable[g].points - 2
+        playertable[h].points = playertable[h].points - 2
+        playertable[i].points = playertable[i].points + 3
+        print(playertable[g].role + " allied while " + playertable[i].role + " betrayed. " + playertable[g].role + " loses 2 points while " + playertable[i].role + " gains 3.")
+    elif playertable[g].vote == "B" and playertable[i].vote == "A":
+        playertable[g].points = playertable[g].points + 3
+        playertable[h].points = playertable[h].points + 3
+        playertable[i].points = playertable[i].points - 2
+        print(playertable[i].role + " allied while " + playertable[g].role + " betrayed. " + playertable[i].role + " loses 2 points while " + playertable[g].role + " gains 3.")
+    else:
+        print(playertable[g].role + " and " + playertable[i].role + " both betrayed each other. Nothing happened with their scores.")
 def imgChart():
     global p1
     global p2
@@ -505,19 +584,45 @@ def imgChart():
     pg.display.set_caption("Python Ambidex Game")
     global screen
     roundnum = 3
-    layout = "A"
     custom_count = 0
     bg = pg.image.load(("img/chartstuff/blankchart" + layout.upper() + ".png"))
     roundtitle = pg.image.load(("img/chartstuff/r" + str(roundnum) + ".png"))
-    for x in 0,8:
-        custom_count = custom_count + 1
-        if os.path.isfile("img/nameplates/" + playertable[x].name + ".png"):
-            gfxABName1 = pg.image.load(("img/nameplates/" + playertable[x].name + ".png"))
-        else:
-            gfxABName1 = pg.image.load(("img/nameplates/Custom " + str(custom_count) + ".png"))
-        if not os.path.isfile("img/nameplates/" + playertable[0].name + ".png"):
-            gfxABName1 = pg.image.load(("img/nameplates/Custom " + str(custom_count) + ".png"))
-
+    #assigning temp variables due to layout
+    namea = Label
+    namea.text = playertable[0].name
+    namea.center = (250,260)
+    nameb = Label
+    nameb.text = playertable[1].name
+    nameb.center = (370,260)
+    namec = Label
+    namec.text = playertable[2].name
+    namec.center = (480,260)
+    named = Label
+    named.text = playertable[3].name
+    named.center = (600,260)
+    namee = Label
+    namee.text = playertable[4].name
+    namee.center = (710,260)
+    namef = Label
+    namef.text = playertable[5].name
+    namef.center = (830,260)
+    nameg = Label
+    nameg.text = playertable[6].name
+    nameg.center = (950,260)
+    nameh = Label
+    nameh.text = playertable[7].name
+    nameh.center = (1050,260)
+    namei = Label
+    namei.text = playertable[8].name
+    namei.center = (1160,260)
+    for p in playertable:
+        pass
+    nameGroup = pg.sprite.Group(namea,nameb,namec,named,namee,namef,nameg,nameh,namei)
+    sPointsGroup = pg.sprite.Group()
+    voteGroup = pg.sprite.Group()
+    changeGroup = pg.sprite.Group()
+    nPointsGroup = pg.sprite.Group()
+    pointAssignment()
     clock = pg.time.Clock()
     keepGoing = True
     while keepGoing:
@@ -530,8 +635,7 @@ def imgChart():
         # bg.blit(nvlText,nvlText.get_rect())
         pg.mouse.set_visible(True)
         screen.blit(bg, (0, 0))
-        screen.blit(roundtitle, (400, 120))
-        screen.blit(gfxABName1, (207,232))
+        screen.blit(roundtitle, (640, 40))
         pg.display.flip()
 
 
